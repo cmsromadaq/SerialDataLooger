8b911b3748c339dd16b0cc065b6a21a5ca61aba0
CMS Roma DAQ user <cmsroma.daq@gmail.com>
2014-04-19 19:05:07 +0200

    just initial version of DB logging

---
+++ logIntoDB.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import MySQLdb
+from serial_monitor import SerialData as DataGen
+from time import sleep
+
+#establish connection to MySQL. You'll have to change this for your database.
+dbConn = MySQLdb.connect("localhost","database_username","password","database_name") or die ("could not connect to database")
+#open a cursor to the database
+cursor = dbConn.cursor()
+
+device = '/tmp/COM0' #this will have to be changed to the serial port you are using
+try:
+      print "Trying...",device
+      serial = DataGen(device, 9600)
+except:
+      print "Failed to connect on",device
+
+while True: #the infinite loop
+    sleep(10)
+    try:
+        data = serial.next()  #read the data from the arduino (lines are split by \n)
+        pieces = data.split("\t")  #split the data by the tab
+    #Here we are going to insert the data into the Database
+        try:
+            cursor.execute("INSERT INTO weatherData (humidity,tempC) VALUES (%s,%s)", (pieces[0],pieces[1]))
+            dbConn.commit() #commit the insert
+            cursor.close()  #close the cursor
+        except MySQLdb.IntegrityError:
+            print "failed to insert data"
+        finally:
+            cursor.close()  #close just incase it failed
+    except:
+        print "Failed to get data from serial"
